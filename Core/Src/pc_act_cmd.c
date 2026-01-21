2891af2b16062394577e7cd6b578dc55fbdd5ad5
Beszti85 <csababesztercei@gmail.com>
2026-01-21 21:46:23 +0100

    [PC]: add DS1307 SQW command

--- Core/Src/pc_act_cmd.c
+++ Core/Src/pc_act_cmd.c
@@ -6,6 +6,9 @@
  */
 
 #include "pc_act_cmd.h"
+#include "ds1307.h"
+
+extern DS1307_Handler_t DS1307_Handle;
 
 void PC_ExecCmdHandler( uint8_t* ptrRxBuffer, uint8_t* ptrTxBuffer )
 {
@@ -28,6 +31,9 @@ void PC_ExecCmdHandler( uint8_t* ptrRxBuffer, uint8_t* ptrTxBuffer )
     // Start DS1307
     case PC_CMD_DS1307_START:
       //DS1307_Start()
+      break;
+    case PC_CMD_DS1307_CTRL_SQW:
+      DS1307_SquareWaveOutput(&DS1307_Handle, (DS1307_Frequency_e)ptrRxBuffer[0]);
       break;
   }
 }
